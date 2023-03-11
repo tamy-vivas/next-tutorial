82d973fe480c3568aabefa45fcab557ae246ea66
tamy-vivas <tamy.vivas@telusinternational.com>
2023-03-11 11:14:11 -0600

    feat: Head component

--- next-blog/pages/index.js
+++ next-blog/pages/index.js
@@ -1,7 +1,12 @@
+import Head from "next/head";
 function HomePage() {
   console.log("HomePage");
   return (
     <>
+      <Head>
+        <title>Mly Blog</title>
+        <meta name="description" value="This is my blog" />
+      </Head>
       <main>
         <h1>HomePage</h1>
       </main>
