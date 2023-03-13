d6279473df8a39868a6a9dd6650ca1272eb192cc
tamy-vivas <tamy.vivas@telusinternational.com>
2023-03-13 13:00:52 -0600

    feat: Fetching data client-side

---
+++ next-shop/src/pages/index-2.tsx
@@ -0,0 +1,50 @@
+//Option 2: fetch products on the client side
+import Head from 'next/head'
+import { useEffect, useState } from 'react';
+import Title from '../components/Title';
+import { Product, getProducts } from '../lib/product';
+
+
+export const getStaticProps = async () => {
+  console.log('[HomePage] getStaticProps');
+
+  const products = await getProducts();
+  return {
+    props: { products }
+  }
+}
+
+interface HomePageProps {
+  products: Product[];
+}
+
+const Home: React.FC<HomePageProps> = () => {
+
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    //getProducts().then((products)=> setProducts(products));
+    getProducts().then(setProducts);
+  }, [])
+
+
+
+  console.log('[HomePage] render: ', products);
+  return (
+    <>
+      <Head>
+        <title>Next Shop</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="px-6 py-4">
+        <Title>Next Shop</Title>
+        <ul>
+          {
+            products.map((product: Product) => (<li key={product.id}>{product.title}</li>))
+          }
+        </ul>
+      </main>
+    </>
+  )
+}
+
+export default Home;
