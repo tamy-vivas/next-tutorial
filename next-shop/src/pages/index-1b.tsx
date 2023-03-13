c507a458d4fb8b2177d54cdd966d05a8a541810c
tamy-vivas <tamy.vivas@telusinternational.com>
2023-03-13 14:52:17 -0600

    feat: Incremental Static Regeneration

---
+++ next-shop/src/pages/index-1b.tsx
@@ -0,0 +1,42 @@
+//Option 1b: fetch products on the server side 
+//with Incremental Static Regeneration (in getStaticProps)
+import Head from 'next/head'
+import Title from '../components/Title';
+import { Product, getProducts } from '../lib/product';
+
+
+export const getStaticProps = async () => {
+  console.log('[HomePage] getStaticProps');
+
+  const products = await getProducts();
+  return {
+    props: { products },
+    revalidate: 60, //seconds
+  }
+}
+
+interface HomePageProps {
+  products: Product[];
+}
+
+const Home: React.FC<HomePageProps> = ({ products }) => {
+  console.log('[HomePage] render: ', products);
+  return (
+    <>
+      <Head>
+        <title>Next Shop</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="px-6 py-4">
+        <Title>Next Shop</Title>
+        <ul>
+          {
+            products.map((product) => (<li key={product.id}>{product.title}</li>))
+          }
+        </ul>
+      </main>
+    </>
+  )
+}
+
+export default Home;
