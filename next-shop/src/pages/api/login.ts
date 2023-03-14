b743463f6689a4d3e8d01bde8d9295aedb8c6f45
tamy-vivas <tamy.vivas@telusinternational.com>
2023-03-14 17:28:16 -0600

    feat: Login API route

--- next-shop/src/pages/api/login.ts
+++ next-shop/src/pages/api/login.ts
@@ -1,12 +1,24 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { fetchJson } from "../../lib/api";
 
-const handler = (req: NextApiRequest, res: NextApiResponse<any>) => {
+const handler = async (req: NextApiRequest, res: NextApiResponse<any>) => {
   if (req.method !== "POST") {
     res.status(405).end();
     return;
   }
 
-  res.status(200).json({});
+  const { email, password } = req.body;
+
+  try {
+    const { jwt, user } = await fetchJson("http://localhost:1337/auth/local", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ identifier: email, password }),
+    });
+    res.status(200).json({ id: user.id, name: user.username });
+  } catch (error) {
+    res.status(401).end();
+  }
 };
 
 export default handler;
