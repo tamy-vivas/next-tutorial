0f419fd9bb26cd59ba2b6e28ac58f7ee602060e1
tamy-vivas <tamy.vivas@telusinternational.com>
2023-03-14 12:25:47 -0600

    feat: Responsive product page

--- next-shop/src/pages/products/[id].tsx
+++ next-shop/src/pages/products/[id].tsx
@@ -4,6 +4,7 @@ import { ParsedUrlQuery } from 'querystring';
 import Title from '../../components/Title';
 import { Product, getProducts, getProduct } from '../../lib/product';
 import { ApiError } from '../../lib/api';
+import Image from 'next/image';
 
 interface ProductPageParams extends ParsedUrlQuery {
   id: string;
@@ -57,7 +58,18 @@ const ProductPage: React.FC<ProductPageProps> = ({ product }) => {
       </Head>
       <main className="px-6 py-4">
         <Title>{product.title}</Title>
-        <p>{product.description}</p>
+
+        <div className="flex flex-col lg:flex-row">
+          <div >
+            <Image src={product.pictureURL} width={640} height={480} alt="" />
+
+          </div>
+          <div className="flex-1 lg:ml-4">
+            <p className="text-sm"> {product.description}</p>
+            <p className="text-lg font-bold mt-2">{product.price}</p>
+          </div>
+        </div>
+
 
       </main>
     </>
