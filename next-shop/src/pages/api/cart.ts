9eb4249d940df1094ab313825586ffc8bd662a52
tamy-vivas <tamy.vivas@telusinternational.com>
2023-03-16 17:39:48 -0600

    feat: POST Cart handler

--- next-shop/src/pages/api/cart.ts
+++ next-shop/src/pages/api/cart.ts
@@ -1,6 +1,7 @@
 import type { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
 import { fetchJson } from "../../lib/api";
 import { CartItem } from "../../lib/Cart";
+import { Product } from '../../lib/product';
 
 const CMS_URL: string = process.env.CMS_URL || "http://localhost:1337";
 
@@ -16,7 +17,7 @@ const stripCartItem = (cartItem: any): CartItem => {
   };
 };
 
-const handler: NextApiHandler<CartItem[]> = async (
+const handlerGetCart: NextApiHandler<CartItem[]> = async (
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) => {
@@ -36,5 +37,52 @@ const handler: NextApiHandler<CartItem[]> = async (
     res.status(401).end();
   }
 };
+
+
+const handlerPostCart: NextApiHandler<CartItem[]> = async (
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) => {
+  const { jwt } = req.cookies;
+  if (!jwt) {
+    res.status(401).end();
+    return;
+  }
+
+  const {productId, quantity} = req.body;
+
+  try {
+    await fetchJson(`${CMS_URL}/cart-items`, {
+      method: 'POST',
+      headers: { 
+        Authorization: `Bearer ${jwt}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({product: productId, quantity});
+    });
+
+    res.status(200).json({});
+  } catch (error) {
+    res.status(401).end();
+  }
+};
+
+
+//------------------------------------------
+const handler: NextApiHandler = async (
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) => {
+  switch(req.method){
+    case 'GET':
+      return handlerGetCart(req, res);
+    case 'POST':
+      return handlerPostCart(req, res);
+    default:
+      res.status(405).end();
+
+  }
+
+}
 
 export default handler;
